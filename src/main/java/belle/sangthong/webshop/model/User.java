a18ddcedb8bb5298924a80513e084242d71e8844
Belle Sangthong <belle.sangthong@iths.se>
2025-03-11 10:08:37 +0100

    Create User model

---
+++ src/main/java/belle/sangthong/webshop/model/User.java
@@ -0,0 +1,54 @@
+package belle.sangthong.webshop.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String username;
+    private List<Order> orders;
+
+    public User(int id, String username, List<Order> orders) {
+        this.id = id;
+        this.username = username;
+        this.orders = orders;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", orders=" + orders +
+                '}';
+    }
+}
