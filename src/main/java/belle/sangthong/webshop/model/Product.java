42a29bc63ff5890bdef21e5a4d331bd629436de8
Belle Sangthong <belle.sangthong@iths.se>
2025-03-12 14:12:01 +0100

    Add category to product model

--- src/main/java/belle/sangthong/webshop/model/Product.java
+++ src/main/java/belle/sangthong/webshop/model/Product.java
@@ -10,15 +10,16 @@ public class Product {
     private int id;
     private String name;
     private double price;
+    private String category;
 
-    public Product(int id, String name, double price) {
+    public Product(int id, String name, double price, String category) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.category = category;
     }
 
     public Product() {
-
     }
 
     public int getId() {
@@ -45,12 +46,21 @@ public class Product {
         this.price = price;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", price=" + price +
+                ", category='" + category + '\'' +
                 '}';
     }
 }
