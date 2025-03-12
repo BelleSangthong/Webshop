7d9a14db233cda0f9b8f4ab66692227ad45a7430
Belle Sangthong <belle.sangthong@iths.se>
2025-03-12 13:38:09 +0100

    Create product service

---
+++ src/main/java/belle/sangthong/webshop/service/ProductService.java
@@ -0,0 +1,12 @@
+package belle.sangthong.webshop.service;
+import belle.sangthong.webshop.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepo repository;
+
+}
