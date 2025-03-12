4b903a2d8608e691a0c7ec45312ea6be5af03158
Belle Sangthong <belle.sangthong@iths.se>
2025-03-12 13:43:41 +0100

    Create product controller

---
+++ src/main/java/belle/sangthong/webshop/controller/ProductController.java
@@ -0,0 +1,25 @@
+package belle.sangthong.webshop.controller;
+
+import belle.sangthong.webshop.model.Product;
+import belle.sangthong.webshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/product")
+    String getProductPage(Model model) {
+        model.addAttribute("product", new Product());
+        return "productpage";
+    }
+
+
+
+}
