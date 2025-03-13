85da6f934002a897cbe9ba3827e80869422f13ee
Belle Sangthong <belle.sangthong@iths.se>
2025-03-13 08:11:21 +0100

    Try to add more products to database

--- src/main/java/belle/sangthong/webshop/runner/DatabaseRunner.java
+++ src/main/java/belle/sangthong/webshop/runner/DatabaseRunner.java
@@ -15,5 +15,9 @@ public class DatabaseRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         repository.save(new Product("Banan", 12, "Frukt"));
+        repository.save(new Product("Gurka", 20, "Grönsak"));
+        repository.save(new Product("Mjölk", 10, "Mejeri"));
+        repository.save(new Product("Kaffe", 30, "Dryck"));
+
     }
 }
